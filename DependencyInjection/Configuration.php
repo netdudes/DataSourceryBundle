8a0f3bc5894daf6582377d65587133c81b10e452
Adria Lopez <adria@prealfa.com>
2015-01-21 11:53:22 +0100

    Fix bug in bundle DI configuration

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,6 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
+        $treeBuilder->root('netdudes_data_sourcery');
         return $treeBuilder;
     }
 }
