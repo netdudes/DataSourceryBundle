29c74c0d84a9900d7636184a57fe6f4df583b46a
Adria Lopez <adria@prealfa.com>
2015-04-02 17:39:22 +0200

    Refactored tokens

--- UQL/Tokens.php
+++ UQL/Tokens.php
@@ -1,45 +1,12 @@
 <?php
 namespace Netdudes\DataSourceryBundle\UQL;
 
-class Tokens {
+class Tokens
+{
 
-    /**
-     * Terminal tokens of the language defined as ordered non-ambiguous
-     * regular expression matches linked to specific token unique identifiers.
-     */
-    public static $terminalTokens = [
-        "/^(\\()/" => "T_BRACKET_OPEN",
-        "/^(\\))/" => "T_BRACKET_CLOSE",
-        "/^(\\[)/" => "T_ARRAY_OPEN",
-        "/^(\\])/" => "T_ARRAY_CLOSE",
-        "/^(,)/" => "T_ARRAY_SEPARATOR",
-        "/^(not|is not equal to|is not|different to)(\s|$)/i" => "T_OP_NEQ",
-        "/^(!=|<>)/i" => "T_OP_NEQ",
-        "/^(less or equal to|less or equal|before or on)(\s|$)/i" => "T_OP_LTE",
-        "/^(<=)/i" => "T_OP_LTE",
-        "/^(greater or equal to|greater or equal|more or equal to|more or equal|after or on)(\s|$)/i" => "T_OP_GTE",
-        "/^(>=)/i" => "T_OP_GTE",
-        "/^(less than|less|before)(\s|$)/i" => "T_OP_LT",
-        "/^(<)/i" => "T_OP_LT",
-        "/^(larger than|larger|more than|more|after)(\s|$)/i" => "T_OP_GT",
-        "/^(>)/i" => "T_OP_GT",
-        "/^(like|is like)(\s|$)/i" => "T_OP_LIKE",
-        "/^(~)/i" => "T_OP_LIKE",
-        "/^(equal to|equals|on|is|at)(\s|$)/i" => "T_OP_EQ",
-        "/^(==|=)/i" => "T_OP_EQ",
-        "/^(IN)(\s|$)/i" => "T_OP_IN",
-        "/^(AND)(\s|$)/i" => "T_LOGIC_AND",
-        "/^(XOR)(\s|$)/i" => "T_LOGIC_XOR",
-        "/^(OR)(\s|$)/i" => "T_LOGIC_OR",
-        "/^(false)(\s|$)/i" => "T_LITERAL_FALSE",
-        "/^(true)(\s|$)/i" => "T_LITERAL_TRUE",
-        "/^([a-zA-Z0-9]+\([^\(\)]*\))/" => "T_FUNCTION_CALL",
-        "/^([0-9]+|'[^']*'|\"[^\"]*\")/" => "T_LITERAL",
-        "/^([a-zA-Z\\_]+)/" => "T_IDENTIFIER",
-        "/^(\\s+)/" => "T_WHITESPACE",
-    ];
+    public static $terminalTokens;
 
-    public static $tokenCategories =  [
+    public static $tokenCategories = [
         'GROUP_START' => ['T_BRACKET_OPEN'],
         'GROUP_END' => ['T_BRACKET_CLOSE'],
         'ARRAY_START' => ['T_ARRAY_OPEN'],
@@ -89,4 +56,85 @@ class Tokens {
         'T_LITERAL_FALSE' => ['false'],
         'T_LITERAL_TRUE' => ['true']
     ];
+
+    private static $baseTokens = [
+        "/^(\\()/" => "T_BRACKET_OPEN",
+        "/^(\\))/" => "T_BRACKET_CLOSE",
+        "/^(\\[)/" => "T_ARRAY_OPEN",
+        "/^(\\])/" => "T_ARRAY_CLOSE",
+        "/^(,)/" => "T_ARRAY_SEPARATOR",
+        "/^(AND)(\s|$)/i" => "T_LOGIC_AND",
+        "/^(XOR)(\s|$)/i" => "T_LOGIC_XOR",
+        "/^(OR)(\s|$)/i" => "T_LOGIC_OR",
+        "/^(false)(\s|$)/i" => "T_LITERAL_FALSE",
+        "/^(true)(\s|$)/i" => "T_LITERAL_TRUE",
+        "/^([a-zA-Z0-9]+\([^\(\)]*\))/" => "T_FUNCTION_CALL",
+        "/^([0-9]+|'[^']*'|\"[^\"]*\")/" => "T_LITERAL",
+        "/^([a-zA-Z\\_]+)/" => "T_IDENTIFIER",
+        "/^(\\s+)/" => "T_WHITESPACE",
+    ];
+
+    private static $operators = [
+        "STRING_NEQ" => ["!=", "<>", "not", "is not"],
+        "NUMERIC_NEQ" => ["!=", "<>", "not", "is not"],
+        "DATETIME_NEQ" => ["!=", "<>", "not", "is not"],
+        "NUMERIC_LTE" => ["<=", "less or equal to"],
+        "DATETIME_LTE" => ["<=", "before or on"],
+        "NUMERIC_GTE" => [">=", "greater or equal to", "greater or equal than"],
+        "DATETIME_GTE" => [">=", "after or on"],
+        "NUMERIC_LT" => ["<", "less than"],
+        "DATETIME_LT" => ["<", "before"],
+        "NUMERIC_GT" => [">", "greater than", "more than"],
+        "DATETIME_GT" => [">", "after"],
+        "STRING_LIKE" => ["~", "like", "is like"],
+        "STRING_EQ" => ["=", "==", "equal to", "equals", "is"],
+        "NUMERIC_EQ" => ["=", "==", "equal to", "equals", "is"],
+        "DATETIME_EQ" => ["=", "==", "on", "is", "at"],
+        "IN" => ["in"]
+    ];
+
+    private static $operatorTokenAssociations = [
+        "T_OP_EQ" => ["STRING_EQ", "NUMERIC_EQ", "DATETIME_EQ"],
+        "T_OP_GTE" => ["NUMERIC_GTE", "DATETIME_GTE"],
+        "T_OP_GT" => ["NUMERIC_GT", "DATETIME_GT"],
+        "T_OP_LTE" => ["NUMERIC_LTE", "DATETIME_LTE"],
+        "T_OP_LT" => ["NUMERIC_LT", "DATETIME_LT"],
+        "T_OP_LIKE" => ["STRING_LIKE"],
+        "T_OP_IN" => ["IN"]
+    ];
+
+    /**
+     * @var bool
+     */
+    private static $initialised = false;
+
+    public static function initialize()
+    {
+        if (self::$initialised) {
+            return;
+        }
+
+        $tokens = [];
+        foreach (self::$operatorTokenAssociations as $token => $operators) {
+            $strings = [];
+            foreach ($operators as $operator) {
+                $strings = array_merge($strings, self::$operators[$operator]);
+            }
+            $strings = array_unique($strings);
+            usort(
+                $strings,
+                function ($a, $b) {
+                    return strlen($a) - strlen($b);
+                }
+            );
+            $regex = "/^(" . implode("|", $strings) . ")(\s|$)/i";
+            $tokens[$regex] = $token;
+        }
+
+        self::$terminalTokens = array_merge($tokens, self::$baseTokens);
+        self::$initialised = true;
+    }
 }
+
+// Initialise the static class
+Tokens::initialize();
